c5ce3f464150a7884833a27b80b16177ed0fab68
stutithakker <thakkerstuti947@gmail.com>
2026-05-15 21:56:31 +0530

    fix: expand state dropdown to all 28 states + 8 UTs in BankDashboard forms
    - bd-assess and bd-risk manual underwriting forms previously had only 5 states
      (MH, DL, KA, TN, GJ) - now includes all Indian states and Union Territories
    - States grouped with optgroup labels (States / Union Territories)
    - Loan Term 'Custom' option was also already fixed to 'Enter manually...'
      in previous commit - browser refresh required to see update

--- frontend/src/pages/BankDashboard.jsx
+++ frontend/src/pages/BankDashboard.jsx
@@ -1612,7 +1612,46 @@ export default function BankDashboard({ user, onLogout, theme, toggleTheme }) {
 
                     <select className="fselect" value={formData.state} onChange={e => update('state', e.target.value)}>
 
-                      <option value="MH">Maharashtra</option><option value="DL">Delhi</option><option value="KA">Karnataka</option><option value="TN">Tamil Nadu</option><option value="GJ">Gujarat</option>
+                      <optgroup label="States">
+                        <option value="AP">Andhra Pradesh</option>
+                        <option value="AR">Arunachal Pradesh</option>
+                        <option value="AS">Assam</option>
+                        <option value="BR">Bihar</option>
+                        <option value="CG">Chhattisgarh</option>
+                        <option value="GA">Goa</option>
+                        <option value="GJ">Gujarat</option>
+                        <option value="HR">Haryana</option>
+                        <option value="HP">Himachal Pradesh</option>
+                        <option value="JH">Jharkhand</option>
+                        <option value="KA">Karnataka</option>
+                        <option value="KL">Kerala</option>
+                        <option value="MP">Madhya Pradesh</option>
+                        <option value="MH">Maharashtra</option>
+                        <option value="MN">Manipur</option>
+                        <option value="ML">Meghalaya</option>
+                        <option value="MZ">Mizoram</option>
+                        <option value="NL">Nagaland</option>
+                        <option value="OD">Odisha</option>
+                        <option value="PB">Punjab</option>
+                        <option value="RJ">Rajasthan</option>
+                        <option value="SK">Sikkim</option>
+                        <option value="TN">Tamil Nadu</option>
+                        <option value="TS">Telangana</option>
+                        <option value="TR">Tripura</option>
+                        <option value="UP">Uttar Pradesh</option>
+                        <option value="UK">Uttarakhand</option>
+                        <option value="WB">West Bengal</option>
+                      </optgroup>
+                      <optgroup label="Union Territories">
+                        <option value="AN">Andaman &amp; Nicobar Islands</option>
+                        <option value="CH">Chandigarh</option>
+                        <option value="DN">Dadra &amp; Nagar Haveli and Daman &amp; Diu</option>
+                        <option value="DL">Delhi (NCT)</option>
+                        <option value="JK">Jammu &amp; Kashmir</option>
+                        <option value="LA">Ladakh</option>
+                        <option value="LD">Lakshadweep</option>
+                        <option value="PY">Puducherry</option>
+                      </optgroup>
 
                     </select>
 
@@ -3050,7 +3089,46 @@ export default function BankDashboard({ user, onLogout, theme, toggleTheme }) {
 
                     <select className="fselect" value={formData.state} onChange={e => update('state', e.target.value)}>
 
-                      <option value="MH">Maharashtra</option><option value="DL">Delhi</option><option value="KA">Karnataka</option><option value="TN">Tamil Nadu</option><option value="GJ">Gujarat</option>
+                      <optgroup label="States">
+                        <option value="AP">Andhra Pradesh</option>
+                        <option value="AR">Arunachal Pradesh</option>
+                        <option value="AS">Assam</option>
+                        <option value="BR">Bihar</option>
+                        <option value="CG">Chhattisgarh</option>
+                        <option value="GA">Goa</option>
+                        <option value="GJ">Gujarat</option>
+                        <option value="HR">Haryana</option>
+                        <option value="HP">Himachal Pradesh</option>
+                        <option value="JH">Jharkhand</option>
+                        <option value="KA">Karnataka</option>
+                        <option value="KL">Kerala</option>
+                        <option value="MP">Madhya Pradesh</option>
+                        <option value="MH">Maharashtra</option>
+                        <option value="MN">Manipur</option>
+                        <option value="ML">Meghalaya</option>
+                        <option value="MZ">Mizoram</option>
+                        <option value="NL">Nagaland</option>
+                        <option value="OD">Odisha</option>
+                        <option value="PB">Punjab</option>
+                        <option value="RJ">Rajasthan</option>
+                        <option value="SK">Sikkim</option>
+                        <option value="TN">Tamil Nadu</option>
+                        <option value="TS">Telangana</option>
+                        <option value="TR">Tripura</option>
+                        <option value="UP">Uttar Pradesh</option>
+                        <option value="UK">Uttarakhand</option>
+                        <option value="WB">West Bengal</option>
+                      </optgroup>
+                      <optgroup label="Union Territories">
+                        <option value="AN">Andaman &amp; Nicobar Islands</option>
+                        <option value="CH">Chandigarh</option>
+                        <option value="DN">Dadra &amp; Nagar Haveli and Daman &amp; Diu</option>
+                        <option value="DL">Delhi (NCT)</option>
+                        <option value="JK">Jammu &amp; Kashmir</option>
+                        <option value="LA">Ladakh</option>
+                        <option value="LD">Lakshadweep</option>
+                        <option value="PY">Puducherry</option>
+                      </optgroup>
 
                     </select>
 
